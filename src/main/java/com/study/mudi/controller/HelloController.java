c3f73cb0e8493f2cf967f7e7fa7bccf835e9fbd8
luiz <luizgfc@algartelecom.com.br>
2021-04-28 21:00:16 -0300

    aula1

---
+++ src/main/java/com/study/mudi/controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.study.mudi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(HttpServletRequest request) {
+
+        request.setAttribute("name", "world");
+
+        return "hello";
+
+    }
+
+}
