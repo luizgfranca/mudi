ab5540afa7587f38136bc0cee277303f5eb54978
luiz <luizgfc@algartelecom.com.br>
2021-05-02 23:10:58 -0300

    aula3p3

--- src/main/java/com/study/mudi/controller/HomeController.java
+++ src/main/java/com/study/mudi/controller/HomeController.java
@@ -6,12 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
-import java.math.BigDecimal;
-import java.util.Arrays;
 import java.util.List;
 
 @Controller
@@ -23,7 +17,7 @@ public class HomeController {
     @GetMapping("/home")
     public String home(Model model) {
 
-        List<Request> requests = requestRepository.queryAllRequests();
+        List<Request> requests = requestRepository.findAll();
 
         model.addAttribute("orders", requests);
 
--- src/main/java/com/study/mudi/repository/RequestRepository.java
+++ src/main/java/com/study/mudi/repository/RequestRepository.java
@@ -1,25 +1,8 @@
 package com.study.mudi.repository;
 
 import com.study.mudi.model.Request;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
-import java.util.List;
-
 @Repository
-public class RequestRepository {
-
-    @PersistenceContext
-    private EntityManager entityManager;
-
-    public List<Request> queryAllRequests() {
-
-        Query query = entityManager.createQuery("SELECT r FROM Request r", Request.class);
-
-        return query.getResultList();
-
-    }
-
-}
+public interface RequestRepository extends JpaRepository<Request, Long> { }
