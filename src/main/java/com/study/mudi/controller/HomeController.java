e37f269350838fce9455383d4202fbefd61e12dc
luiz <luizgfc@algartelecom.com.br>
2021-05-11 18:45:10 -0300

    aula7p2

--- src/main/java/com/study/mudi/controller/HomeController.java
+++ src/main/java/com/study/mudi/controller/HomeController.java
@@ -1,20 +1,26 @@
 package com.study.mudi.controller;
 
 import com.study.mudi.model.Request;
+import com.study.mudi.model.RequestStatus;
 import com.study.mudi.repository.RequestRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 import java.util.List;
 
 @Controller
+@RequestMapping("/home")
 public class HomeController {
 
     @Autowired
     private RequestRepository requestRepository;
 
-    @GetMapping("/home")
+    @GetMapping
     public String home(Model model) {
 
         List<Request> requests = requestRepository.findAll();
@@ -22,6 +28,29 @@ public class HomeController {
         model.addAttribute("orders", requests);
 
         return "home";
+    }
+
+    @GetMapping("/{status}")
+    public String homeByStatus(
+            @PathVariable("status") String status,
+            Model model
+    ) {
+
+        List<Request> requests = requestRepository.findByStatus(
+                RequestStatus.valueOf(status.toUpperCase())
+        );
+
+        model.addAttribute("orders", requests);
+        model.addAttribute("status", status);
+
+        return "home";
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public String onError() {
+
+        return "redirect:/home";
+
     }
 
 }
--- src/main/java/com/study/mudi/repository/RequestRepository.java
+++ src/main/java/com/study/mudi/repository/RequestRepository.java
@@ -1,8 +1,15 @@
 package com.study.mudi.repository;
 
 import com.study.mudi.model.Request;
+import com.study.mudi.model.RequestStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface RequestRepository extends JpaRepository<Request, Long> { }
+public interface RequestRepository extends JpaRepository<Request, Long> {
+
+    List<Request> findByStatus(RequestStatus waiting);
+
+}
